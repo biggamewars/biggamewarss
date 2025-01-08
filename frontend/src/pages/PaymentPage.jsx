bff17de1978f2fc998ec136e5665d151956a7e04
biggamewars <biggamewars@gmail.com>
2025-01-08 17:27:46 +0530

    Update PaymentPage.jsx

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -17,7 +17,7 @@ const PaymentPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/payment', {
+      const response = await fetch('https://biggamewarss.onrender.com/payment', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
