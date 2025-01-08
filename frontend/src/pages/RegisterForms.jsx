1debee25b19d6d52fe2fba89d2396f92041e04b2
biggamewars <biggamewars@gmail.com>
2025-01-08 17:28:03 +0530

    Update RegisterForms.jsx

--- frontend/src/pages/RegisterForms.jsx
+++ frontend/src/pages/RegisterForms.jsx
@@ -63,7 +63,7 @@ const RegisterForms = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await fetch('http://localhost:5000/submit-data', {
+      await fetch('https://biggamewarss.onrender.com/submit-data', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
